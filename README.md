a9bfb2e3128d50bdf41fc7803a215966a5fa20fb
Vino Rodrigues <github@m.vinorodrigues.com>
2018-03-16 17:21:59 +1100

    rel 1

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 
 MantisBT Plugin to put back what the "Modern UI" build broke.
 
+Bringing back the ability to use the following `config.php` settings:
+```php
+$g_top_include_page = '%absolute_path%/config/top.php';
+$g_bottom_include_page = '%absolute_path%/config/bottom.php';
+$g_meta_include_file = '%absolute_path%/config/meta.php';
+```
--- mbt_customize.php
+++ mbt_customize.php
@@ -19,24 +19,44 @@ class mbt_customizePlugin extends MantisPlugin {
 
 	function hooks() {
 		return array(
-			// 'EVENT_PLUGIN_INIT' => 'mbtp_event_plugin_init',
+			'EVENT_PLUGIN_INIT' => 'mbtp_event_plugin_init',
 			// 'EVENT_LAYOUT_CONTENT_BEGIN' => 'mbtp_event_layout_content_begin',
-			'EVENT_LAYOUT_BODY_BEGIN' => 'mbtp_event_layout_body_begin',
 			'EVENT_LAYOUT_RESOURCES' =>  'mbtp_event_layout_resources',
+			'EVENT_LAYOUT_BODY_BEGIN' => 'mbtp_event_layout_body_begin',
 			'EVENT_LAYOUT_BODY_END' =>   'mbtp_event_layout_body_end',
 		);
 	}
 
-	function mbtp_event_layout_body_begin( $p_event ) {
-		echo '<!-- EVENT_LAYOUT_BODY_BEGIN -->';
+	function mbtp_event_plugin_init( $p_event ) {
+		global $g_meta_include_file, $g_top_include_page, $g_bottom_include_page;
+		if( !isset($g_meta_include_file) ) $g_meta_include_file = '';
+		if( !isset($g_top_include_page) ) $g_top_include_page = '';
+		if( !isset($g_bottom_include_page) ) $g_bottom_include_page = '';
 	}
 
 	function mbtp_event_layout_resources( $p_event ) {
-		echo '<!-- EVENT_LAYOUT_RESOURCES -->';
+		$t_meta = config_get_global( 'meta_include_file' );
+		if( !is_blank( $t_meta )  && file_exists( $t_meta ) && !is_dir( $t_meta ) ) {
+			include( $t_meta );
+		}
+
+		event_signal( 'EVENT_LAYOUT_PAGE_HEADER' );
+	}
+
+	function mbtp_event_layout_body_begin( $p_event ) {
+		$t_page = config_get( 'top_include_page' );
+
+		if( !is_blank( $t_page ) && file_exists( $t_page ) && !is_dir( $t_page ) ) {
+			include( $t_page );
+		}
 	}
 
 	function mbtp_event_layout_body_end( $p_event ) {
-		echo '<!-- EVENT_LAYOUT_BODY_END -->';
+		$t_page = config_get( 'bottom_include_page' );
+
+		if( !is_blank( $t_page ) && file_exists( $t_page ) && !is_dir( $t_page ) ) {
+			include( $t_page );
+		}
 	}
 
 }
